430831b667c5eb465ade36bfd8c0589bfd6885d5
Daryl Corbin <corbindac@gmail.com>
2020-01-08 23:36:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = {
-    name: "Grass Hopper"
+    name: "Grace Hopper"
   }
   pioneer
 end
